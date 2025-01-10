a82ee230b509ebe296a49718b122546b3c4ca19a
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-01-10 13:05:09 -0500

    1.0.1 Update
    -Fixed update cart quantity bug

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,8 @@
-export const cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 3,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2,
-  },
-];
+export const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(pId) {
   const quantity = Number(
@@ -21,9 +16,13 @@ export function addToCart(pId) {
         quantity: quantity,
       })
     : (cart[index].quantity += quantity);
+
+  saveToStorage();
 }
 
 export function removeFromCart(pId) {
   const index = cart.findIndex((p) => p.productId == pId);
   index !== -1 && cart.splice(index, 1);
+
+  saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,6 +3,8 @@ import { products } from "../data/products.js";
 import formatCurrency from "./utils/money.js";
 
 $(document).ready(function () {
+  updateCartQuantity();
+
   let productsHTML = "";
   products.forEach((product) => {
     const stars = product.rating.stars * 10;
